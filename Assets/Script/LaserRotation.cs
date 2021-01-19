890549ca635fd3bee1ae003e3f399fbb3fbf53cb
jooyoung cha <45927229+colpang@users.noreply.github.com>
2021-01-19 22:45:02 +0900

    점멸 적용

--- Assets/Script/LaserRotation.cs
+++ Assets/Script/LaserRotation.cs
@@ -8,8 +8,6 @@ public class LaserRotation : MonoBehaviour
     float angle;
     float stopAngle;
     Vector2 target, mouse;
-    public bool check = true;
-    float timer=0;
     Color color;
 
 
@@ -25,38 +23,15 @@ public class LaserRotation : MonoBehaviour
         angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
         //this.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
 
-        if (Input.GetMouseButtonDown(0)&&check)
+        if (Input.GetMouseButtonDown(0))
         {
             stopAngle = angle;
             this.transform.rotation = Quaternion.AngleAxis(stopAngle - 90, Vector3.forward);
-            check = false;
-            timer = 5.0f;
-            StartCoroutine(WaitForIt());
-            timer = 0;
         }
 
 
     }
-    IEnumerator WaitForIt()
-    {
-        while(timer > 1.0f)
-        {
-            timer -= Time.deltaTime;
-            appear();
-            yield return new WaitForSeconds(0.02f);
-        }
-
-        check = true;
-    }
-
-    public void appear()
-    {
-        SpriteRenderer spr = GetComponent<SpriteRenderer>();
-        Color color = spr.color;
-        color.a = 1f;
-        spr.color = color;
-
-    }
+   
 
     public void disappear()
     {
